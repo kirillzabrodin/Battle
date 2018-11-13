1902348411a9b232898a47e1999b69bd31d9ed89
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-11-13 16:26:47 +0000

    new styling

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -1,13 +1,6 @@
 require_relative '../../app.rb'
 
-feature "prints some words" do
-  scenario "on homepage" do
-    visit("/")
-    expect(page).to have_content("Testing insfrastructure is as clear as London weather")
-  end
-end
-
-feature "gets names and displays them" do
+feature "gets names" do
   scenario "Player 1 on the homepage" do
     visit("/")
     expect(page).to have_field('P1:')
@@ -15,5 +8,20 @@ feature "gets names and displays them" do
   scenario "Player 2 on the homepage" do
     visit("/")
     expect(page).to have_field('P2:')
+  end
+end
+
+feature "displays name" do
+  scenario "Player 1 enters" do
+    visit("/")
+    fill_in("P1", with: "Kirill")
+    click_button("Submit")
+    expect(page).to have_content "Kirill"
+  end
+  scenario "Player 2 enters" do
+    visit("/")
+    fill_in("P2", with: "Chris")
+    click_button("Submit")
+    expect(page).to have_content "Chris"
   end
 end
