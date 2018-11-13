60eadd4192caa0411c1f71827b8d8d1244c2b6e6
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-11-13 22:43:37 +0000

    Skinny controllers

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "sinatra/base"
-require "./lib/player"
+require './lib/game'
+require './lib/player'
 
 class Battle < Sinatra::Base
 
@@ -10,22 +11,20 @@ class Battle < Sinatra::Base
   end
 
   post "/names" do
-    $player_1 = Player.new(params[:p1])
-    $player_2 = Player.new(params[:p2])
+    player_1 = Player.new(params[:p1])
+    player_2 = Player.new(params[:p2])
+    $game = Game.new(player_1, player_2)
     redirect "/play"
   end
 
   get "/play" do
-    @p1 = $player_1
-    @p2 = $player_2
+    @game = $game
     erb :play
   end
 
   get "/attack" do
-
-    @p1 = $player_1
-    @p2 = $player_2
-    Game.new.attack(@p2)
+    @game = $game
+    @game.attack(@game.player_2)
     erb :attack
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,12 +2,26 @@ require 'game'
 
 describe Game do
 
-  let(:kirill) { double :kirill }
+  let(:kirill) { double :kirill, receive_damage: true }
+  let(:chris) { double :chris, receive_damage: true }
+  subject(:game) { described_class.new(kirill, chris) }
 
   describe '#attack' do
     it 'damages the player' do
       expect(kirill).to receive(:receive_damage)
       subject.attack(kirill)
+    end
+  end
+
+  describe '#player_1' do
+    it 'retrieves the first player' do
+      expect(game.player_1).to eq kirill
+    end
+  end
+
+  describe '#player_2' do
+    it 'retrieves the second player' do
+      expect(game.player_2).to eq chris
     end
   end
 end
